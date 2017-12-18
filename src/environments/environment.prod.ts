b14dcb0dfaf70ff543ea4db80bc911a056f96243
dianamcnulty <dianamcnulty@gmail.com>
2017-12-18 11:05:37 -0500

    update apiServer to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiServer: 'http://localhost:4741' //change when pushed to prod
+  // apiServer: 'http://localhost:4741' //change when pushed to prod
+  apiServer: 'https://cloutierphotography.herokuapp.com'
 };
