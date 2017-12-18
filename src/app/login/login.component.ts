d0be00b5793351e8b65319351be215ed5e816a0c
dianamcnulty <dianamcnulty@gmail.com>
2017-12-18 11:17:54 -0500

    removed auto-signup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
 
 
   ngOnInit() {
-    this.auth.signUp('admin', 'Dragon', 'Dragon')
+    // this.auth.signUp('admin', 'Dragon', 'Dragon')
   }
 
   signOut() {
